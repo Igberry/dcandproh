8439f6468e01e4b540f1e0bc42ecc75285d4265f
Igberry <davidigberichuks@gmail.com>
2025-06-19 12:30:04 +0100

    commit

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === 'true',
+  secure: process.env.SMTP_SECURE === 'false',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
